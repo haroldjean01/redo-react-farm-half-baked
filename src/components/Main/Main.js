48ab59e123f952bee8f498e3488b6f0c2fbb98b9
Harold Jean <haroldjean38@yahoo.com>
2023-01-30 12:28:32 -0500

    mapped through array in Main.js

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import './Main.css';
+import { animals } from '../../data.js';
+import Animal from '../Animal/Animal.js';
 
 export default function Main() {
-  return <main></main>;
+  return (
+    <main className="main">
+      {animals.map((animal) => (
+        <Animal
+          key={animal.type}
+          name={animal.type}
+          url={animal.url}
+          says={animal.says}
+          top={animal.top}
+          left={animal.left}
+        />
+      ))}
+    </main>
+  );
 }
